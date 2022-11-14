9257dbc944d1c37796cc0b01516638a074af59e9
Daniel Unda <danielxolalpaunda@gmail.com>
2022-11-14 12:57:04 -0600

    New features

--- web-server/main.py
+++ web-server/main.py
@@ -1,5 +1,22 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
+app=FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1,2,3]
+
+@app.get('/contact')
+def get_dict():
+    return {'name':'daniel'}
+
+@app.get('/HTML')
+def get_html():
+    return """
+        <h1> Soy una pagina</h1>
+    """
 
 def run():
     store.get_catehories()
